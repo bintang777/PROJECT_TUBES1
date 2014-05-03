f31cba3cd4e5070aa80edbfb4dba918fab925d31
bintang777 <kblade8@gmail.com>
2014-05-03 13:44:05 +0700

    Update news.php
    telah memasukan berita terbaru dari teknologi yang akan menghebohkan dunia di tahun 2014

--- news.php
+++ news.php
@@ -1,38 +1,46 @@
 <?php include "connection.php"; ?>
 <html>
 <head>
-<title>JURUSAN TEKNIK INFORMATIKA UNIVERSITAS TASIKMALAYA</title>
+<title>TEKNOLOGI NEWS</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
+ 3 Teknologi Terbaru 2014 yang Akan Menghebohkan Dunia
 
-<table width="798" height="665"  border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr>
-    <td colspan="3" align="center" valign="top" bgcolor="#24A2D3" height="150"><img src="images/banner.jpg" width="798" height="148"></td>
-  </tr>
-  <tr>
-    <td colspan="3" align="center" valign="top" bgcolor="#24A2D3" height="30"><?php include "topmenu.php"; ?></td>
-  </tr>
-  <tr>
-    <td width="170" height="435" valign="top"><?php include "leftmenu.php"; ?></td>
-    <td width="468" align="center" valign="top" background="images/bg_center.gif"><table width="95%"  border="0" cellpadding="0" cellspacing="0" class="mini-font">
-        <tr>
-          <td height="39" valign="top" background="images/bg_header.gif"><h3>NEWS</h3></td>
-        </tr>
-        <tr>
-          <td valign="top"><div align="justify"><span class="headline">Perbedaan Jurusan Ilmu Komputer, Teknik Informatika, Sistem Informasi dan Teknik</span><br>
-            Pertama, Jurusan Ilmu Komputer itu adalah jurusan yang mempelajari segala hal yang berkaitan dengan komputer, baik dari segi Hardware maupun softwarenya.Bagi yang berminat di bidang Hardware lebih baik mengambil jurusan teknik komputer, sedangkan bagi yang lebih berminat pada bidang software dapat memperdalam di jurusan Teknik Informatika.. <a href="shownews.php" class="left-text">[selengkapnya...]</a> <br>
-            ..............................................................................................................
-          </div></td>
-        </tr>
-    </table></td>
-    <td width="160" valign="top"><?php include "rightmenu.php"; ?></td>
-  </tr>
-  <tr align="center">
-    <td colspan="3"><?php include "footer.php"; ?></td>
-  </tr>
-</table>
+Tahun 2012 lalu, diisi dengan maraknya berbagai peluncuran perangkat-perangkat mobile canggih dengan spesifikasi yang mampu memanjakan pengguna. Mulai dari Galaxy S III, hingga iPhone 5. Belum lagi kemunculan ponsel generasi terbaru seperti HTC Droid DNA yang menawarkan layar resolusi Full HD 1080 x 1920 dengan kerapatan 441 ppi. Tahun 2013 juga dimeriahkan oleh peluncuran sistem operasi Windows 8 dari Microsoft, yang diikuti oleh kemunculan berbagai jenis produk tablet dan komputer yang telah menyematkan OS terbaru tersebut. Sekelumit teknologi yang disebutkan tersebut, merupakan perkembangan yang telah dapat dirasakan pada tahun 2012 lalu.
+
+Lalu bagaimana dengan tahun 2014?
+
+
+Berikut beberapa teknologi terbaru 2014 yang disinyalir akan menghebohkan dunia :
+
+
+1. APLLE TV
+
+
+samsung telah mngeluarkan smart tv apple tv pun juga tak mau ikut kalah meluncurkan smart tv
+
+kabar yang menyebutkan apple akan membuat televisi (TV) sendiri semakin berhembus kencang
+
+kabar terbaru menyebutkan, smart TV keluaran apple ini akan berukuran antara 46 hingga 55 inci. menurut sumber dari foxconn yang merupakan mitra manufaktur apple. saat ini produsen elektronik terbesar dari dunia itu telah membuat prototipe dari model yang merupakan hasil kolaborasi foxconn dan sharp.
+
+
+2. GOOGLE GLASS
+
+
+Teknologi terbaru 2013 adalah google glass. dan yang di atas adalah gambar google glass beserta logonya. google memperkenalkan kacamata canggih yang saat ini masih bernama Project Glass. kaca mata ini membawa kemapuan komputasi yang cukup menakjubkan. kaca mata ini menggunakan kontrol suara dan bisa di gunakan untuk pencarian online. email dan navigasi arah di samping kaca mata. bahkan juga bisa di gunakan via chat dan obrolan.
+
+
+kaca mata yang berbasis teknologi ”augmented reality” ini tampaknya memakai sistem operasi android. meskipun kabar ini belum di pastikan. kaca mata yang kerap di namakan google glass ini di banderol seharga $1.500 atau sekitar Rp 14 juta. di ketahui, project glass di buat oleh tim di laboratorium terbesar di dunia itu telah membuat prototipe dari model yang merupakan hasil kolaborasi foxconn dan sharp.
+
+
+3. PLAYSTATION 4
+
+
+sementara konsol playstation 3 sudah memasuki revisi ketiga, kabarnya kini sony sedang mempersiapkan konsol generasi selanjutnya yang disinyalir sebagai playstation 4 . produk ini kabarnya akan menggunakan prosesor A10 milik AMD yang memiliki teknologi di mana CPU dan GPU dapat bekerja bersamaan dan di dukung RAM berkapasitas 8 GB ataupun 16 GB untuk media penyimpanan kabarnya akan berkapasitas 256 GB, namun masih belum jelas apakah akan menggunakan SSD atau Harddisk. produk ini juga akan di sertakan sebuah blu-ray drive dan koneksi WiFi. playstation 4 atau yang saat ini masih menggunakan sebutan orbis rencananya akan di perkenalkan sebelum even electronic entertainment Expo (E3) yang akan di gelar tahun ini pangeran wiliam pun Ngebet ingin play station 4. 
+<!--Dokumen ini berasal dari blog-->
+<Blog="http://lodzcity.blogspot.com/2014/01/tahun-2012-lalu-diisi-dengan-maraknya.html”>
 </body>
 </html>
